cbb28f9397d1a25a3d3e9c4530cedafda05457ea
mrtzaj <mortezajamali4241@gmail.com>
2020-03-18 13:43:18 -0700

    Working on Routing !

--- index.php
+++ index.php
@@ -3,18 +3,13 @@ require 'vendor/autoload.php';
 
 use ACIDECore\App\View;
 
-$router = new AltoRouter();
-$router->map( 'GET', '/users', function() {
-    echo 'This is home page';
-});
-$match = $router->match();
+$klein = new \Klein\Klein();
 
-if( is_array($match) && is_callable( $match['target'] ) ) {
-    call_user_func_array( $match['target'], $match['params'] );
-} else {
-    // no route was matched
-    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-}
+$klein->respond('GET', '/', function () {
+    return 'Hello World!';
+});
+
+$klein->dispatch();
 
 //View::display('index');
 ?>
