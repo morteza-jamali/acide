df8d97d163611b5acac804a89d724f7fd51b175b
mrtzaj <mortezajamali4241@gmail.com>
2020-05-31 09:25:45 -0700

    exportRepositories updated !

--- assets/js/app.js
+++ assets/js/app.js
@@ -47220,6 +47220,26 @@ var repositories = {
     url: 'https://github.com/morteza-jamali/angularjs-project/archive/master.zip',
     img: 'assets/img/icons/angular.svg',
     name: 'AngularJS'
+  },
+  3: {
+    url: 'https://github.com/sahat/hackathon-starter/archive/master.zip',
+    img: 'assets/img/icons/nodejs.svg',
+    name: 'NodeJS'
+  },
+  4: {
+    url: 'https://github.com/kriasoft/react-starter-kit/archive/master.zip',
+    img: 'assets/img/icons/react.svg',
+    name: 'React'
+  },
+  5: {
+    url: 'https://github.com/Ismaestro/angular9-example-app/archive/master.zip',
+    img: 'assets/img/icons/angular.svg',
+    name: 'Angular'
+  },
+  6: {
+    url: 'https://github.com/sindresorhus/electron-boilerplate/archive/master.zip',
+    img: '',
+    name: 'Electron'
   }
 };
 /* harmony default export */ __webpack_exports__["default"] = (repositories);
