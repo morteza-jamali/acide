c75843c9b9dc246d9ddf37eb8ea5872a21a7981f
morteza-jamali <mortezajamali4241@gmail.com>
2021-04-22 01:10:22 +0430

    feat: website created

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/acide', '<rootDir>/libs/app-ui'],
+  projects: [
+    '<rootDir>/apps/acide',
+    '<rootDir>/libs/app-ui',
+    '<rootDir>/apps/website',
+  ],
 };
