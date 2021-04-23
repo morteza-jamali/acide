c74c76f53a884541e3d03a09d61272c4c411640e
morteza-jamali <mortezajamali4241@gmail.com>
2021-04-23 15:10:02 +0430

    feat: website routing system created

---
+++ apps/website/pages/home.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente atque rerum, est repudiandae quae eos laboriosam soluta ipsum voluptatem molestias consequatur obcaecati nihil distinctio. Odio soluta doloremque nesciunt dignissimos nihil.</p>
+</body>
+
+</html>
